2422c930464e88372c8e53546ee8e13a98ba3771
Dmytro Golovchenko <dmytro.golovchenko@gmail.com>
2023-09-14 22:23:49 -0700

    Add support of DB name in --database param of update_database.py

--- update_database.py
+++ update_database.py
@@ -3,11 +3,13 @@
 
 import argparse
 import logging
+import sys
+from pathlib import Path
 
 import file_database
 
 
-def main():
+def main(argv):
     """Module as util use wrapper."""
     arg_parser = argparse.ArgumentParser(
         description='Load files to database')
@@ -15,22 +17,29 @@ def main():
                             default=None)
     arg_parser.add_argument(
         '--max_depth',
+        type=int,
         help='Number of levels below media to process, 0 for single level',
         default=None)
+    arg_parser.add_argument(
+        '--database',
+        type=Path,
+        help='Database file',
+        required=False,
+        default=file_database.DEFAULT_DATABASE_NAME)
     arg_parser.add_argument('-v', '--verbose',
                             help='Print verbose output',
                             action='count', default=0)
     arg_parser.add_argument('--dry_run',
                             help='Print action instead of executing it',
                             action="store_true", default=False)
-    args = arg_parser.parse_args()
+    args = arg_parser.parse_args(argv[1:])
     logging.basicConfig(
         format="%(asctime)s [%(levelname)s] %(message)s",
         level=logging.WARNING - 10 * (args.verbose if args.verbose < 3 else 2))
 
-    with file_database.FileManagerDatabase() as file_db:
+    with file_database.FileManagerDatabase(db_name=args.database) as file_db:
         file_db.update_dir(args.media, max_depth=args.max_depth)
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
