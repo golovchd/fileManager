325bfe7fc520c1537493ff2ba5aa5bffe2bb7bc5
Dmytro Golovchenko <dmytro.golovchenko@gmail.com>
2023-12-10 19:45:19 -0800

    Convert duplicate_dirs into dataclass
    Also added profiling in compare_dirs() and abstructed queries into functions

--- check_duplicates.py
+++ check_duplicates.py
@@ -4,7 +4,9 @@
 import argparse
 import logging
 import sys
+from dataclasses import dataclass
 from pathlib import Path
+from time import CLOCK_MONOTONIC, clock_gettime_ns
 from typing import Any, Dict, List, Set, Tuple
 
 from file_database import DEFAULT_DATABASE, FileManagerDatabase
@@ -32,6 +34,37 @@ SELECT_DIR_FILES = ("SELECT `fsrecords`.`ROWID`, `FileId`, `ParentId`, "
 SELECT_SUBDIRS = ("SELECT `ROWID`, `Name`, `ParentId` FROM `fsrecords` "
                   "WHERE `ParentId` IN (?, ?) AND `FileId` IS NULL "
                   "ORDER BY `ParentId`, `Name`")
+SELECT_COPIES_IN_DIR = ("SELECT `FileId`, `ParentId`, `FileSize`,"
+                        "GROUP_CONCAT(`fsrecords`.`ROWID`)"
+                        "FROM `fsrecords`"
+                        "INNER JOIN `files` ON `files`.`ROWID` = A.`FileId`"
+                        "GROUP BY `ParentId`, `FileId`, `FileSize`"
+                        "ORDER BY `ParentId`, `FileId`")
+
+
+@dataclass
+class DirsPair:
+    DirAId: int
+    DirBId: int
+
+    def __hash__(self) -> int:
+        return hash(f"{self.DirAId},{self.DirBId}")
+
+
+@dataclass
+class FileInfo:
+    FileId: int
+    FileSize: int
+
+    def __hash__(self) -> int:
+        return self.FileId
+
+
+@dataclass
+class DirsDifference:
+    files_a_not_b: Dict[int, int]
+    files_b_not_a: Dict[int, int]
+    matching_size: int
 
 
 class FileDuplicates(FileManagerDatabase):
@@ -41,16 +74,38 @@ class FileDuplicates(FileManagerDatabase):
             self, db_path: Path, min_size: int):
         super().__init__(db_path, 0)
         self._min_size = min_size
-        self.checked_dirs: Dict[str, bool] = {}
+        self.compare_count = 0
+        self.checked_dirs: Dict[DirsPair, bool] = {}
         self.empty_dirs: Set[int] = set()  # Dirs w/o files in any subdirs
         self.non_empty_dirs: Set[int] = set()
-        self.duplicate_dirs: Dict[str, Tuple[List[Tuple[int, int]],
-                                             List[Tuple[int, int]],
-                                             int]] = {}
+        self.duplicate_dirs: Dict[DirsPair, DirsDifference] = {}
 
-    def mark_as_checked(self, dir_a: int, dir_b: int, match: bool) -> bool:
-        self.checked_dirs[f"{dir_a},{dir_b}"] = match
-        self.checked_dirs[f"{dir_b},{dir_a}"] = match
+    def process_profile(
+            self, dir_a: int, dir_b: int, match: bool,
+            start_time: int, query_time: int,
+            size_time: int, dirs_files_matches_time: int,
+            end_time: int, point: int,
+            a_count: int, b_count: int) -> None:
+        query_ms = (query_time - start_time) // 1000
+        size_ms = (size_time - query_time) // 1000
+        match_ms = (dirs_files_matches_time - size_time) // 1000
+        compare_ms = (end_time - dirs_files_matches_time) // 1000
+        qps = (a_count + b_count) * 1000 // query_ms
+        mcps = max(a_count, b_count) * 1000 // (
+            compare_ms + match_ms + size_ms)
+        tcps = (a_count + b_count) * 1000 // (
+            compare_ms + match_ms + size_ms)
+        logging.debug(
+            f"{dir_a}({a_count}),{dir_b}({b_count}) {match}[{point}], Times: "
+            f"{query_ms}/{size_ms}/{match_ms}/{compare_ms} ms, {qps} QPS, "
+            f"{mcps} CPS, {tcps} TCPS")
+
+    def mark_as_checked(
+            self, dir_a: int, dir_b: int, match: bool, profile: List[Any]
+            ) -> bool:
+        self.checked_dirs[DirsPair(dir_a, dir_b)] = match
+        self.checked_dirs[DirsPair(dir_b, dir_a)] = match
+        self.process_profile(dir_a, dir_b, match, *profile)
         return match
 
     def compare_subdirs(
@@ -67,13 +122,25 @@ class FileDuplicates(FileManagerDatabase):
             return True
         return False
 
-    def compare_dirs(self, dir_a: int, dir_b: int, max_diff: int) -> bool:
-        """Compares dirs by SHA1 of files in them."""
-        if f"{dir_a},{dir_b}" in self.checked_dirs:
-            logging.debug(f"Dir pair {dir_a},{dir_b} was tested before")
-            return self.checked_dirs[f"{dir_b},{dir_a}"]
-        dir_a_files = []
-        dir_b_files = []
+    def query_files(
+            self, dir_a: int, dir_b: int
+            ) -> Tuple[Dict[int, int], Dict[int, int]]:
+        """Queries subdirs of given pair of dirs."""
+        a_file_size: Dict[int, int] = {}
+        b_file_size: Dict[int, int] = {}
+
+        for row in self._exec_query(
+                SELECT_DIR_FILES, (dir_a, dir_b), commit=False):
+            if int(row[2]) == int(dir_a):
+                a_file_size[row[1]] = row[3]
+            else:
+                b_file_size[row[1]] = row[3]
+        return a_file_size, b_file_size
+
+    def query_subdirs(
+            self, dir_a: int, dir_b: int
+            ) -> Tuple[List[FileInfo], List[FileInfo]]:
+        """Queries files of given pair of dirs."""
         dir_a_subdirs = []
         dir_b_subdirs = []
         for row in self._exec_query(
@@ -82,62 +149,94 @@ class FileDuplicates(FileManagerDatabase):
                 dir_a_subdirs.append(row)
             else:
                 dir_b_subdirs.append(row)
+        return dir_a_subdirs, dir_b_subdirs
+
+    def compare_dirs(self, dir_a: int, dir_b: int, max_diff: int) -> bool:
+        """Compares dirs by SHA1 of files in them."""
+        if DirsPair(dir_a, dir_b) in self.checked_dirs:
+            logging.debug(f"Dir pair {dir_a},{dir_b} was tested before")
+            return self.checked_dirs[DirsPair(dir_a, dir_b)]
+        start_time = clock_gettime_ns(CLOCK_MONOTONIC)
+        self.compare_count += 1
 
         dir_a_path = self.get_path(dir_a)
         dir_b_path = self.get_path(dir_b)
         logging.debug(
             f"comparing Dir pair {dir_a_path},{dir_b_path}"
             f" ({dir_a},{dir_b}) for max {max_diff} diff files")
-        for row in self._exec_query(
-                SELECT_DIR_FILES, (dir_a, dir_b), commit=False):
-            if int(row[2]) == int(dir_a):
-                dir_a_files.append((row[1], row[3]))
-            else:
-                dir_b_files.append((row[1], row[3]))
-        matching_size = sum([
-            file[1] for file in dir_a_files if file in dir_b_files
-        ])
+        dir_a_subdirs, dir_b_subdirs = self.query_subdirs(dir_a, dir_b)
+        a_file_size, b_file_size = self.query_files(dir_a, dir_b)
+        query_time = clock_gettime_ns(CLOCK_MONOTONIC)
 
-        if dir_a_files == dir_b_files:
+        matching_size = sum([
+            a_file_size[file] for file in a_file_size if file in b_file_size
+        ])
+        size_time = clock_gettime_ns(CLOCK_MONOTONIC)
+
+        dirs_files_matches: bool = (a_file_size.keys() == b_file_size.keys())
+        dirs_files_matches_time = clock_gettime_ns(CLOCK_MONOTONIC)
+
+        if dirs_files_matches:
             logging.info(f"Dir {dir_a_path} is matching "
                          f"{dir_b_path} with 0 differences")
             log_subdirs_info(
                 dir_a_path, dir_b_path, dir_a_subdirs, dir_b_subdirs)
-            self.duplicate_dirs.update({
-                f"{dir_a},{dir_b}": ([], [], matching_size)})
-            return self.mark_as_checked(dir_a, dir_b, True)
+            self.duplicate_dirs[DirsPair(dir_a, dir_b)] = DirsDifference(
+                {}, {}, matching_size)
+            end_time = clock_gettime_ns(CLOCK_MONOTONIC)
+            return self.mark_as_checked(
+                dir_a, dir_b, True,
+                [start_time, query_time, size_time, dirs_files_matches_time,
+                 end_time, 1, len(a_file_size), len(b_file_size)])
 
-        a_not_b = [a for a in dir_a_files if a not in dir_b_files]
-        b_not_a = [b for b in dir_b_files if b not in dir_a_files]
-        diff_count = len(a_not_b) + len(b_not_a)
+        a_not_b_size: Dict[int, int] = {
+            a: a_file_size[a] for a in a_file_size if a not in b_file_size}
+        b_not_a_size: Dict[int, int] = {
+            b: b_file_size[b] for b in b_file_size if b not in a_file_size}
+        diff_count = len(a_not_b_size) + len(b_not_a_size)
 
         if (diff_count > max_diff and
-                len(a_not_b) > max_diff and
-                len(b_not_a) > max_diff):
+                len(a_not_b_size) > max_diff and
+                len(b_not_a_size) > max_diff):
             logging.debug(
                 f"Dir {dir_a_path} different from {dir_b_path}"
                 f" with {diff_count} differences")
-            return self.mark_as_checked(dir_a, dir_b, False)
+            end_time = clock_gettime_ns(CLOCK_MONOTONIC)
+            return self.mark_as_checked(
+                dir_a, dir_b, False,
+                [start_time, query_time, size_time, dirs_files_matches_time,
+                 end_time, 2, len(a_file_size), len(b_file_size)])
 
-        if not a_not_b:
+        if not a_not_b_size:
             logging.info(f"Files of dir {dir_a_path} contained "
                          f"in {dir_b_path}")
             log_subdirs_info(
                 dir_a_path, dir_b_path, dir_a_subdirs, dir_b_subdirs)
-            self.duplicate_dirs.update({
-                f"{dir_a},{dir_b}": (a_not_b, b_not_a, matching_size)})
-            return self.mark_as_checked(dir_a, dir_b, True)
+            self.duplicate_dirs[DirsPair(dir_a, dir_b)] = DirsDifference(
+                a_not_b_size, b_not_a_size, matching_size)
+            end_time = clock_gettime_ns(CLOCK_MONOTONIC)
+            return self.mark_as_checked(
+                dir_a, dir_b, True,
+                [start_time, query_time, size_time, dirs_files_matches_time,
+                 end_time, 3, len(a_file_size), len(b_file_size)])
 
-        if not b_not_a:
+        if not b_not_a_size:
             logging.info(f"Files of {dir_b_path} "
                          f"contained in {dir_a_path}")
             log_subdirs_info(
                 dir_a_path, dir_b_path, dir_a_subdirs, dir_b_subdirs)
-            self.duplicate_dirs.update({
-                f"{dir_b},{dir_a}": (b_not_a, a_not_b, matching_size)})
-            return self.mark_as_checked(dir_a, dir_b, True)
+            self.duplicate_dirs[DirsPair(dir_b, dir_a)] = DirsDifference(
+                a_not_b_size, b_not_a_size, matching_size)
+            end_time = clock_gettime_ns(CLOCK_MONOTONIC)
+            return self.mark_as_checked(
+                dir_a, dir_b, True,
+                [start_time, query_time, size_time, dirs_files_matches_time,
+                 end_time, 4, len(a_file_size), len(b_file_size)])
 
-        result = {f"{dir_a},{dir_b}": (a_not_b, b_not_a, matching_size)}
+        result = {
+            DirsPair(dir_a, dir_b): DirsDifference(
+                a_not_b_size, b_not_a_size, matching_size)
+        }
         if diff_count <= max_diff:
             logging.info(
                 f"Dir {dir_a_path} is matching "
@@ -145,57 +244,75 @@ class FileDuplicates(FileManagerDatabase):
             log_subdirs_info(
                 dir_a_path, dir_b_path, dir_a_subdirs, dir_b_subdirs)
             self.duplicate_dirs.update(result)
-            return self.mark_as_checked(dir_a, dir_b, True)
-        if len(a_not_b) <= max_diff:
+            end_time = clock_gettime_ns(CLOCK_MONOTONIC)
+            return self.mark_as_checked(
+                dir_a, dir_b, True,
+                [start_time, query_time, size_time, dirs_files_matches_time,
+                 end_time, 5, len(a_file_size), len(b_file_size)])
+        if len(a_not_b_size) <= max_diff:
             logging.info(f"Files of {dir_a_path} contained in "
                          f"{dir_b_path} with "
-                         f"all but {len(a_not_b)} files")
+                         f"all but {len(a_not_b_size)} files")
             log_subdirs_info(
                 dir_a_path, dir_b_path, dir_a_subdirs, dir_b_subdirs)
             self.duplicate_dirs.update(result)
-            return self.mark_as_checked(dir_a, dir_b, True)
-        if len(b_not_a) <= max_diff:
+            end_time = clock_gettime_ns(CLOCK_MONOTONIC)
+            return self.mark_as_checked(
+                dir_a, dir_b, True,
+                [start_time, query_time, size_time, dirs_files_matches_time,
+                 end_time, 6, len(a_file_size), len(b_file_size)])
+        if len(b_not_a_size) <= max_diff:
             logging.info(f"Files of {dir_b_path} contained in "
                          f"{dir_a_path} with "
-                         f"all but {len(b_not_a)} files")
+                         f"all but {len(b_not_a_size)} files")
             log_subdirs_info(
                 dir_a_path, dir_b_path, dir_a_subdirs, dir_b_subdirs)
             self.duplicate_dirs.update(result)
-            return self.mark_as_checked(dir_a, dir_b, True)
+            end_time = clock_gettime_ns(CLOCK_MONOTONIC)
+            return self.mark_as_checked(
+                dir_a, dir_b, True,
+                [start_time, query_time, size_time, dirs_files_matches_time,
+                 end_time, 7, len(a_file_size), len(b_file_size)])
 
         logging.warning(
             f"Dir {dir_a_path} different from {dir_b_path}"
             f" with {diff_count} differences")
-        return self.mark_as_checked(dir_a, dir_b, False)
+        end_time = clock_gettime_ns(CLOCK_MONOTONIC)
+        return self.mark_as_checked(
+            dir_a, dir_b, False,
+            [start_time, query_time, size_time, dirs_files_matches_time,
+             end_time, 8, len(a_file_size), len(b_file_size)])
 
     def sort_output_duplicate_dirs(self, max_diff: int) -> None:
         sorted_duplicates = sorted(
             self.duplicate_dirs.keys(),
-            key=lambda pair: self.duplicate_dirs[pair][2], reverse=True)
+            key=lambda pair: self.duplicate_dirs[pair].matching_size,
+            reverse=True)
         for dir_pair in sorted_duplicates:
-            pair = dir_pair.split(",")
-            if not (self.duplicate_dirs[dir_pair][0] or
-                    self.duplicate_dirs[dir_pair][1]):
+            if not (self.duplicate_dirs[dir_pair].files_a_not_b or
+                    self.duplicate_dirs[dir_pair].files_b_not_a):
                 status = "full_match"
-            elif not self.duplicate_dirs[dir_pair][0]:
+            elif not self.duplicate_dirs[dir_pair].files_a_not_b:
                 status = "left_in_right"
-            elif not self.duplicate_dirs[dir_pair][1]:
+            elif not self.duplicate_dirs[dir_pair].files_b_not_a:
                 status = "right_in_left"
-            elif (len(self.duplicate_dirs[dir_pair][0]) +
-                  len(self.duplicate_dirs[dir_pair][1])) <= max_diff:
+            elif (len(self.duplicate_dirs[dir_pair].files_a_not_b) +
+                  len(self.duplicate_dirs[dir_pair].files_b_not_a)
+                  ) <= max_diff:
                 status = f"mismatch_under_{max_diff}"
-            elif len(self.duplicate_dirs[dir_pair][0]) <= max_diff:
+            elif len(self.duplicate_dirs[dir_pair].files_a_not_b) <= max_diff:
                 status = f"left_mismatch_right_under_{max_diff}"
-            elif len(self.duplicate_dirs[dir_pair][1]) <= max_diff:
+            elif len(self.duplicate_dirs[dir_pair].files_b_not_a) <= max_diff:
                 status = f"right_mismatch_left_under_{max_diff}"
             else:
                 status = "unexpected"
 
-            print(f"{self.duplicate_dirs[dir_pair][2]} B {status}: "
-                  f"'{self.get_path(int(pair[0]))}' "
-                  f"'{self.get_path(int(pair[1]))}'")
+            print(f"{self.duplicate_dirs[dir_pair].matching_size} B {status}: "
+                  f"'{self.get_path(dir_pair.DirAId)}' "
+                  f"'{self.get_path(dir_pair.DirBId)}'")
 
     def search_duplicate_folders(self, max_diff: int) -> None:
+        start_time = clock_gettime_ns(CLOCK_MONOTONIC)
         for row in self._exec_query(DUPLICATES_FOLDERS,
                                     (self._disk_id, self._min_size),
                                     commit=False):
@@ -207,7 +324,15 @@ class FileDuplicates(FileManagerDatabase):
                     if dirs[i] == dirs[j]:
                         continue
                     self.compare_dirs(dirs[i], dirs[j], max_diff)
+        query_time = clock_gettime_ns(CLOCK_MONOTONIC)
         self.sort_output_duplicate_dirs(max_diff)
+        sort_time = clock_gettime_ns(CLOCK_MONOTONIC)
+        logging.info(f"Query time {query_time - start_time} ns, "
+                     f"Sort time {sort_time - query_time}, "
+                     f"Compare_count={self.compare_count}, "
+                     f"checked_dirs_count={len(self.checked_dirs)}, "
+                     f"duplicate_dirs_count={len(self.duplicate_dirs)}, "
+                     )
 
 
 def log_subdirs_info(
