cf664295c082d667e3e0e078f9a0d24992b5b0fe
Dmytro Golovchenko <dmytro.golovchenko@luminartech.com>
2026-03-12 11:56:27 -0700

    SW-29771: Retry first and then ignore DB errors to insert individual files

--- file_database_update.py
+++ file_database_update.py
@@ -1,10 +1,10 @@
 """File database update module."""
+from __future__ import annotations
 
 import logging
+from sqlite3 import IntegrityError
 from time import CLOCK_MONOTONIC, clock_gettime_ns
-from typing import List, Optional, Tuple
 
-import file_utils
 from file_database import FileManagerDatabase
 from storage_client import StorageClient
 
@@ -12,14 +12,13 @@ _MAX_ORFAN_SEARCH_DEPTH = 256
 
 
 class FileDatabaseUpdater(FileManagerDatabase):
-    def update_file(self, file_full_name: str, storage_client: StorageClient) -> Tuple[int, int, int]:
+    def update_file(self, file_full_name: str, storage_client: StorageClient) -> tuple[int, int, int]:
         """Updates or inserts in DB details on file.
             Returns hashed size, file size, hash time in ns."""
         if not self._cur_dir_id:
             raise ValueError("Missing _cur_dir_id")
         (
-            fsrecord_id, sha1_read_date, db_file_mtime, file_id, db_file_size,
-            db_file_sha1
+            fsrecord_id, sha1_read_date, db_file_mtime, file_id, db_file_size, db_file_sha1
         ) = self.get_db_file_info(file_full_name)
 
         if storage_client.slow_file_read:
@@ -46,16 +45,25 @@ class FileDatabaseUpdater(FileManagerDatabase):
             return 0, size, 0   # Skip file if unable to read
 
         logging.debug(f"Update fsrecord {fsrecord_id} from {storage_client.media}/{file_full_name}, "
-                      f"SHA1={sha1}, {size} B, {file_name} {file_type}")
-        new_file_id = self.select_update_file_record(
-            sha1, mtime, size, file_name, file_type
-        )
-        self.update_fsrecord(fsrecord_id, file_full_name, mtime, new_file_id)
-        # TODO: Handle deletion of old `files` record file_id
-        del file_id
+                    f"SHA1={sha1}, {size} B, {file_name} {file_type}")
+        for attempt in range(3):
+            try:
+                new_file_id = self.select_update_file_record(
+                    sha1, mtime, size, file_name, file_type
+                )
+                self.update_fsrecord(fsrecord_id, file_full_name, mtime, new_file_id)
+                # TODO: Handle deletion of old `files` record file_id
+                del file_id
+                return size, size, hash_time
+            except IntegrityError as error:
+                logging.warning(f"update_file: Attempt {attempt + 1} failed to insert file record for {storage_client.media}/{file_full_name}, due to {error}, retrying...")
+                continue
+
+        logging.error(f"update_file: Failed to insert file record for {storage_client.media}/{file_full_name} after 3 attempts, ")
         return size, size, hash_time
 
-    def update_files(self, files: List[str], storage_client: StorageClient) -> Tuple[int, int, int]:
+
+    def update_files(self, files: list[str], storage_client: StorageClient) -> tuple[int, int, int]:
         """Updating files in current dir.
             Returns hashed size, file size, hash time in ns."""
         hashed_size = 0
@@ -107,8 +115,8 @@ class FileDatabaseUpdater(FileManagerDatabase):
 
     def update_dir(
             self, storage_client: StorageClient,
-            max_depth: Optional[int] = 0, check_disk: bool = True
-            ) -> Tuple[int, int, int, int]:
+            max_depth: int | None = 0, check_disk: bool = True
+            ) -> tuple[int, int, int, int]:
         """Updating DB with dir details, entrypoint for update_database."""
         dir_path = storage_client.media
         start_time = clock_gettime_ns(CLOCK_MONOTONIC)
