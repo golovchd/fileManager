ab6858f1e7a60d1da64a04dc82b1bc2047fec8cb
Dmytro <103075413+dmytro-golovchenko-luminartech@users.noreply.github.com>
2026-03-13 16:01:30 -0700

    SW-29771: Execute update_files in threads (#32)
    1. Added `--threads` param to `update_database.py`
    2. `SQLite3connection` supports `get_connection()` method to provide
    thread-specific connection to DB
    3. `SQLite3connection._exec_query()` accepts optional argument
    `connection` and using it instead of default if it was provided
    4. Retry mechanism improved in `SQLite3connection._exec_query()` (higher
    retry counts and random first delay) to increase reliability in
    multiprocessor, multi-threads scenarios
    5. In `FileDatabaseUpdater` storage_client is now class attribute
    6. `FileDatabaseUpdater.update_files()` runs
    `ThreadPoolExecutor.map(self.update_file, files)` instead of loop over
    them
    7. `FileDatabaseUpdater.update_file()`
    8. Methods in `FileManagerDatabase` accepts connection to pass is into
    `_exec_query()` call to work in multi-threads scenarios

--- db_utils.py
+++ db_utils.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import logging
+import random
 import sqlite3
 from pathlib import Path
 from time import sleep
@@ -21,8 +22,9 @@ TABLE_COMPARE: dict[str, list[int]] = {
     "fsrecords": [4, 7],
 }
 
-_RETRY_COUNT = 3
+_RETRY_COUNT = 10
 _RETRY_FIRST_DELAY = 1
+_RETRY_RANDOM_DELAY = 0.2   # Ensure spread of retries for multi thread scenarios
 _RETRY_DELAY_EXP = 1.5
 
 
@@ -76,8 +78,9 @@ class SQLite3connection:
     """Basic sqlite3 coonection functionality."""
 
     def __init__(self, db_path: Path):
+        self._db_path = db_path
         initialize_database(db_path)
-        self._con = sqlite3.connect(db_path, timeout=10)
+        self._con = self.get_connection()
         self.SQLITE_LIMIT_VARIABLE_NUMBER = max(sqlite3.SQLITE_LIMIT_VARIABLE_NUMBER, 32766) if hasattr(sqlite3, 'SQLITE_LIMIT_VARIABLE_NUMBER') else 32766
         logging.info(f"Using DB {db_path}")
 
@@ -91,24 +94,45 @@ class SQLite3connection:
         if self._con:
             self._con.close()
 
-    def _exec_query(self, sql: str, params: Sequence, commit=True):
+    def get_connection(self) -> sqlite3.Connection:
+        """Returns the sqlite3 connection."""
+        return sqlite3.connect(self._db_path, timeout=10)
+
+    def _exec_query(self, sql: str, params: Sequence, commit: bool=True, connection: sqlite3.Connection | None = None):
         """SQL quesy executor with logging."""
         delay: float = _RETRY_FIRST_DELAY
+        total_delay: float = 0
+        working_con = connection if connection else self._con
+
         for retry in range(_RETRY_COUNT):
             try:
-                result = self._con.execute(sql, params)
+                result = working_con.execute(sql, params)
                 if commit:
-                    self._con.commit()
+                    working_con.commit()
                 logging.debug("SQL succeed: %s with %r", sql, params)
+                if retry > _RETRY_COUNT // 2:
+                    logging.warning(
+                        "SQL succeed after %d retries and total delay %f.2 sec: %s with %r", retry, total_delay, sql, params)
                 return result
             except sqlite3.OperationalError as e:
                 if (str(e) == "database is locked"):
                     logging.debug(
                         "SQL failed as %s, retry %d after %ds: %s with %r",
                         e, retry, delay, sql, params)
+                    if retry > _RETRY_COUNT // 2:
+                        logging.warning(
+                            "DB locked after %d retries and total delay %f.2 sec: %s with %r", retry, total_delay, sql, params)
+                    if not retry:   # Add random delay for first retry to ensure spread of retries for multi thread scenarios
+                        delay += random.random() * _RETRY_RANDOM_DELAY
                     sleep(delay)
+                    total_delay += delay
                     delay *= _RETRY_DELAY_EXP
                     continue
                 logging.exception(
                     "SQL failed as %s: %s with %r", e, sql, params)
                 raise e
+        else:
+            logging.error(
+                "SQL failed after %d retries: %s with %r", _RETRY_COUNT, sql, params)
+            raise sqlite3.OperationalError(
+                f"SQL failed after {_RETRY_COUNT} retries: {sql} with {params}")
--- file_database_update.py
+++ file_database_update.py
@@ -2,6 +2,8 @@
 from __future__ import annotations
 
 import logging
+from concurrent.futures import ThreadPoolExecutor
+from pathlib import Path
 from sqlite3 import IntegrityError
 from time import CLOCK_MONOTONIC, clock_gettime_ns
 
@@ -12,72 +14,89 @@ _MAX_ORFAN_SEARCH_DEPTH = 256
 _FILE_INSERT_RETRY_COUNT = 2
 
 
+
 class FileDatabaseUpdater(FileManagerDatabase):
-    def update_file(self, file_full_name: str, storage_client: StorageClient) -> tuple[int, int, int]:
+    def __init__(self, db_path: Path, rehash_time: float, threads: int, storage_client: StorageClient):
+        super().__init__(db_path, rehash_time)
+        self.threads = threads
+        self.storage_client = storage_client
+
+    def update_file(self, file_full_name: str) -> tuple[int, int, int]:
         """Updates or inserts in DB details on file.
             Returns hashed size, file size, hash time in ns."""
         if not self._cur_dir_id:
             raise ValueError("Missing _cur_dir_id")
-        (
-            fsrecord_id, sha1_read_date, db_file_mtime, file_id, db_file_size, db_file_sha1
-        ) = self.get_db_file_info(file_full_name)
+        if self.storage_client.is_symlink(file_full_name):
+            logging.warning("update_file called for symlink %s/%s", self._cur_dir_path, file_full_name)
+            return 0, 0, 0
 
-        if storage_client.slow_file_read:
+        db_connection = self.get_connection()   # Using dedicated connection for each thread to avoid locking issues
+        fsrecord_id, sha1_read_date, db_file_mtime, file_id, db_file_size, db_file_sha1 = self.get_db_file_info(file_full_name, connection=db_connection)
+
+        if self.storage_client.slow_file_read:
             if fsrecord_id and db_file_sha1:
-                logging.debug(f"update_file: slow read, skipping hashing for {storage_client.media}/{file_full_name}, " +
+                logging.debug(f"update_file: slow read, skipping hashing for {self.storage_client.media}/{file_full_name}, " +
                               f"SHA1 in DB {db_file_sha1}, skipped update of fsrecord_id={fsrecord_id}")
+                db_connection.close()
                 return 0, db_file_size, 0
         else:
-            file_name, file_type, size, mtime, _, _ = storage_client.read_file_info(file_full_name, False)
+            file_name, file_type, size, mtime, _, _ = self.storage_client.read_file_info(file_full_name, False)
             if size == 0 or file_name == "" and file_type == "":
-                logging.warning(f"update_file: size {size} for {storage_client.media}/{file_full_name}, " +
+                logging.warning(f"update_file: size {size} for {self.storage_client.media}/{file_full_name}, " +
                                 (f"skipping update of fsrecord_id={fsrecord_id}" if fsrecord_id else f"skipping insertion into DB"))
+                db_connection.close()
                 return 0, 0, 0
 
             if (self._rehash_time < sha1_read_date and
                     db_file_size == size and db_file_mtime == mtime):
+                db_connection.close()
                 return 0, size, 0   # Current file details matching DB, no re-hash
 
-        file_name, file_type, size, mtime, sha1, hash_time = storage_client.read_file_info(file_full_name, True)
+        file_name, file_type, size, mtime, sha1, hash_time = self.storage_client.read_file_info(file_full_name, True)
         if not sha1:
             logging.warning(f"Failed to get SHA1 for {file_full_name}, "
                             f"SHA1 in DB {db_file_sha1}, skipped  " +
                             (f"update of fsrecord_id={fsrecord_id}" if fsrecord_id else "insertion into DB"))
+            db_connection.close()
             return 0, size, 0   # Skip file if unable to read
 
-        logging.debug(f"Update fsrecord {fsrecord_id} from {storage_client.media}/{file_full_name}, "
+        logging.debug(f"Update fsrecord {fsrecord_id} from {self.storage_client.media}/{file_full_name}, "
                       f"SHA1={sha1}, {size} B, {file_name} {file_type}")
         for attempt in range(_FILE_INSERT_RETRY_COUNT):
             try:
                 new_file_id = self.select_update_file_record(
-                    sha1, mtime, size, file_name, file_type
+                    sha1, mtime, size, file_name, file_type, connection=db_connection
                 )
-                self.update_fsrecord(fsrecord_id, file_full_name, mtime, new_file_id)
+                self.update_fsrecord(fsrecord_id, file_full_name, mtime, new_file_id, connection=db_connection)
                 # TODO: Handle deletion of old `files` record file_id
                 del file_id
+                db_connection.close()
                 return size, size, hash_time
             except IntegrityError as error:
-                logging.warning(f"update_file: Attempt {attempt + 1} failed to insert file record for {storage_client.media}/{file_full_name}, due to {error}, retrying...")
+                logging.warning(f"update_file: Attempt {attempt + 1} failed to insert file record for {self.storage_client.media}/{file_full_name}, due to {error}, retrying...")
                 continue
 
-        logging.error(f"update_file: Failed to insert file record for {storage_client.media}/{file_full_name} after {_FILE_INSERT_RETRY_COUNT} attempts, ")
+        logging.error(f"update_file: Failed to insert file record for {self.storage_client.media}/{file_full_name} after {_FILE_INSERT_RETRY_COUNT} attempts, ")
+        db_connection.close()
         return size, size, hash_time
 
-    def update_files(self, files: list[str], storage_client: StorageClient) -> tuple[int, int, int]:
+    def update_files(self, files: list[str]) -> tuple[int, int, int]:
         """Updating files in current dir.
             Returns hashed size, file size, hash time in ns."""
         hashed_size = 0
         total_size = 0
         total_hash_time = 0
-        for file_name in files:
-            if storage_client.is_symlink(file_name):
-                logging.warning("update_files called for symlink %s/%s",
-                                self._cur_dir_path, file_name)
-                continue
-            hashsed, size, hash_time = self.update_file(file_name, storage_client)
+        logging.debug(f"update_files: Starting processing {len(files)} files under {self.storage_client.media} in {self.threads} threads")
+        start_time_ns = clock_gettime_ns(CLOCK_MONOTONIC)
+        with ThreadPoolExecutor(max_workers=self.threads) as executor:
+            results = executor.map(self.update_file, files)
+        process_time_ns = clock_gettime_ns(CLOCK_MONOTONIC) - start_time_ns
+        logging.debug(f"update_files: Processed {len(files)} files under {self.storage_client.media} in {self.threads} threads in {process_time_ns / 1E9:.2f} sec")
+        for hashsed, size, hash_time in results:
             hashed_size += hashsed
             total_size += size
             total_hash_time += hash_time
+
         return hashed_size, total_size, total_hash_time
 
     def print_statistic(
@@ -89,13 +108,14 @@ class FileDatabaseUpdater(FileManagerDatabase):
         process_time_ns = clock_gettime_ns(CLOCK_MONOTONIC) - start_time_ns
         average_process_speed = (files_total_size * 1E3) / process_time_ns
         file_process_speed = (files_count * 1E9) / process_time_ns
+        used_threads = min(self.threads, files_count) if files_count else 1
         if files_hash_time_ns:
-            average_hash_time = (files_hashed_size * 1E3) / files_hash_time_ns
+            average_hash_speed = (files_hashed_size * 1E3) / (files_hash_time_ns / used_threads)
         else:
-            average_hash_time = 0
+            average_hash_speed = 0
         hashing_size_pct = (100 * files_hashed_size / files_total_size
                             if files_total_size else 0)
-        hashing_time_pct = 100 * files_hash_time_ns / process_time_ns
+        hashing_time_pct = 100 * files_hash_time_ns / (process_time_ns * used_threads)
         logging.info(f"Processed {path} in {process_time_ns / 1E9:.2f} sec, "
                      f"{files_count} files, {file_process_speed:.2f} files/sec, "
                      f"total size {files_total_size / 1E6:.2f} MB, "
@@ -104,47 +124,41 @@ class FileDatabaseUpdater(FileManagerDatabase):
                      f"{hashing_time_pct:.1f}% by time, "
                      f"{files_hashed_size / 1E6:.2f} MB in "
                      f"{files_hash_time_ns / 1E9:.2f} sec, "
-                     f"{average_hash_time:.2f} MB/sec.")
+                     f"{average_hash_speed:.2f} MB/sec.")
 
-    def set_cur_dir(self, storage_client: StorageClient) -> None:
-        """Saving/updating dir with a path to disk root."""
+    def set_cur_dir(self) -> None:
+        """Saving/updating dir with a path to disk root. COUNT NOT BE CALLED IN CONCURRENT EXECUTION."""
         if not self._top_dir_id:
             raise ValueError("Missing _top_dir_id")
-        self._cur_dir_id = self.get_dir_id(storage_client.get_path_from_mount())
-        self._cur_dir_path = storage_client.media
+        self._cur_dir_id = self.get_dir_id(self.storage_client.get_path_from_mount())
+        self._cur_dir_path = self.storage_client.media
         logging.debug(f"set_cur_dir: {self._cur_dir_path}={self._cur_dir_id}")
 
-    def update_dir(
-            self, storage_client: StorageClient,
-            max_depth: int | None = 0, check_disk: bool = True
-            ) -> tuple[int, int, int, int]:
+    def update_dir( self, max_depth: int | None = 0, check_disk: bool = True) -> tuple[int, int, int, int]:
         """Updating DB with dir details, entrypoint for update_database."""
-        dir_path = storage_client.media
+        dir_path = self.storage_client.media
         start_time = clock_gettime_ns(CLOCK_MONOTONIC)
         logging.debug(f"update_dir for {dir_path}")
         if check_disk:
-            self.set_disk(**storage_client.get_disk_info())
-        self.set_cur_dir(storage_client)
-        files, sub_dirs = storage_client.read_dir()
+            self.set_disk(**self.storage_client.get_disk_info())
+        self.set_cur_dir()
+        files, sub_dirs = self.storage_client.read_dir()
         files_count = len(files)
         logging.info("update_dir: %s, %d files, %d sub dirs",
                      dir_path, len(files), len(sub_dirs))
         self.clean_cur_dir(files, is_files=True)
         self.clean_cur_dir(sub_dirs, is_files=False)
-        files_hashed_size, files_total_size, files_hash_time_ns = (
-            self.update_files(files, storage_client))
-
+        files_hashed_size, files_total_size, files_hash_time_ns = (self.update_files(files))
 
         if max_depth != 0:
             for sub_dir_name in sub_dirs:
-                storage_client.set_media(dir_path  + '/' + sub_dir_name)
-                if storage_client.is_symlink():
+                self.storage_client.set_media(dir_path  + '/' + sub_dir_name)
+                if self.storage_client.is_symlink():
                     logging.warning("update_dir called for symlink %s/%s",
                                     dir_path, sub_dir_name)
                     continue
                 dir_files_count, dir_hashed_size, dir_size, hash_time_ns = (
                     self.update_dir(
-                        storage_client,
                         max_depth=max_depth - 1 if max_depth else None,
                         check_disk=False))
                 files_count += dir_files_count
