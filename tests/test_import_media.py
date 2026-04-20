fe75efa13c68483abd9c5d035725ca2fcf015497
Dmytro Golovchenko <dmytro.golovchenko@gmail.com>
2026-04-19 18:44:28 -0700

    drop unittest from tests/test_import_media.py

--- tests/test_import_media.py
+++ tests/test_import_media.py
@@ -2,12 +2,10 @@
 from __future__ import annotations
 
 import re
-import unittest
 from datetime import datetime
 from pathlib import Path
 
 import pytest
-from pyfakefs import fake_filesystem_unittest  # type: ignore
 
 from file_manager.import_config import MediaConfig  # type: ignore
 from file_manager.import_media import ExifTimeError  # type: ignore
@@ -19,53 +17,53 @@ TEST_SCRIPT_DIR = Path(__file__).resolve().parent
 TEST_DATA_DIR = TEST_SCRIPT_DIR.parent / "test_data"
 
 
-class ImportMediaFakeFsTestCase(fake_filesystem_unittest.TestCase):
-    """ Fake FS based tests. """
-    def setUp(self):
-        self.setUpPyfakefs()
+@pytest.mark.parametrize(
+    "file_path, type",
+    [
+        (Path('test.JPG'), 'jpg'),
+        (Path('test.mpg'), 'mpg'),
+        (Path('dir/test.mpg'), 'mpg'),
+        (Path('dir/test.rx'), 'rx'),
+        (Path('dir/test'), ''),
+        (Path('/dir/path/'), ''),
+    ]
+)
+def test_file_type(file_path: Path, type: str) -> None:
+    assert file_type_from_name(file_path) == type
 
-    def test_file_type(self):
-        self.assertEqual(file_type_from_name(Path('test.JPG')), 'jpg')
-        self.assertEqual(file_type_from_name(Path('test.mpg')), 'mpg')
-        self.assertEqual(file_type_from_name(Path('dir/test.mpg')), 'mpg')
-        self.assertEqual(file_type_from_name(Path('dir/test.rx')), 'rx')
-        self.assertEqual(file_type_from_name(Path('dir/test')), '')
-        self.assertEqual(file_type_from_name(Path('/dir/path/')), '')
 
-
-class ImportMediaTestCase(unittest.TestCase):
-    """ Tests with real FS from test_data."""
-    def test_exif_time2unix(self):
-        with self.assertRaises(ExifTimeError):
-            exif_time2unix('2018:06:30')
-        with self.assertRaises(ExifTimeError):
-            test_file_date = read_file_time(
-                TEST_DATA_DIR / 'media/not_an_image', True)
+def test_exif_time2unix() -> None:
+    with pytest.raises(ExifTimeError):
+        exif_time2unix('2018:06:30')
+    with pytest.raises(ExifTimeError):
         test_file_date = read_file_time(
-            TEST_DATA_DIR / 'media/DSC06979.JPG', True)
-        self.assertEqual(test_file_date, datetime(2018, 2, 19, 11, 5, 43))
+            TEST_DATA_DIR / 'media/not_an_image', True)
+    test_file_date = read_file_time(
+        TEST_DATA_DIR / 'media/DSC06979.JPG', True)
+    assert test_file_date == datetime(2018, 2, 19, 11, 5, 43)
 
-    def test_import(self):
-        missing_list = [
-            '6TB-2 benchmark 2018-08-25 20-58-29.png',
-            'IMG_0013.JPG',
-        ]
-        present_dict = {
-            'DSC06979.JPG': TEST_DATA_DIR / 'storage/DSC06979.JPG',
-            'IMG_0004.JPG': TEST_DATA_DIR / 'storage/tagged/IMG_0004.JPG',
-        }
-        present_tagged_dict = {
-            'DSC06979.JPG': TEST_DATA_DIR / 'storage/tagged/DSC06979.JPG',
-            'IMG_0004.JPG': TEST_DATA_DIR / 'storage/tagged/IMG_0004.JPG',
-        }
-        not_imported, already_imported = get_import_list(
-            TEST_DATA_DIR / 'media', TEST_DATA_DIR / 'storage', [])
-        self.assertEqual(sorted(missing_list), sorted(not_imported))
-        self.assertEqual(present_dict, already_imported)
-        not_imported, already_imported = get_import_list(
-            TEST_DATA_DIR / 'media', TEST_DATA_DIR / 'storage/tagged', [])
-        self.assertEqual(sorted(missing_list), sorted(not_imported))
-        self.assertEqual(present_tagged_dict, already_imported)
+
+def test_get_import_list() -> None:
+    missing_list = [
+        '6TB-2 benchmark 2018-08-25 20-58-29.png',
+        'IMG_0013.JPG',
+    ]
+    present_dict = {
+        'DSC06979.JPG': TEST_DATA_DIR / 'storage/DSC06979.JPG',
+        'IMG_0004.JPG': TEST_DATA_DIR / 'storage/tagged/IMG_0004.JPG',
+    }
+    present_tagged_dict = {
+        'DSC06979.JPG': TEST_DATA_DIR / 'storage/tagged/DSC06979.JPG',
+        'IMG_0004.JPG': TEST_DATA_DIR / 'storage/tagged/IMG_0004.JPG',
+    }
+    not_imported, already_imported = get_import_list(
+        TEST_DATA_DIR / 'media', TEST_DATA_DIR / 'storage', [])
+    assert sorted(missing_list) == sorted(not_imported)
+    assert present_dict == already_imported
+    not_imported, already_imported = get_import_list(
+        TEST_DATA_DIR / 'media', TEST_DATA_DIR / 'storage/tagged', [])
+    assert sorted(missing_list) == sorted(not_imported)
+    assert present_tagged_dict, already_imported
 
 
 root_empty_config = MediaConfig("root", re.compile("^$"), [])
